59f5446c6439594673346e0fab7960e97483d0fa
karmartir <karmartir@gmail.com>
2025-12-12 23:11:46 -0500

    Mobile Menu

--- js/script.js
+++ js/script.js
@@ -1,31 +1,44 @@
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
 const input = document.getElementById("link-input");
 const linkForm = document.getElementById("link-form");
 const err = document.getElementById("err-msg");
 
+btn.addEventListener("click", navToggle);
+
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+}
+
 linkForm.addEventListener("submit", formSubmit);
 
 // Validate a URL
-function validURL(str){
-    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-      '(\\:\\d+)?(/[-a-z\\d%_.~+]*)*'+ // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-    return !!pattern.test(str);
+function validURL(str) {
+  var pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // fragment locator
+  return !!pattern.test(str);
 }
 
 function formSubmit(e) {
-    e.preventDefault();
-    if (input.value === "") {
-        err.innerHTML = "Please add a link";
-        input.classList.add("border-red");
-    } else if (!validURL(input.value)) {
-        err.innerHTML = "Please add a valid link";
-        input.classList.add("border-red");
-    } else {
-        err.innerHTML = "";
-        input.classList.remove("border-red");
-        alert('Success')
-    }
+  e.preventDefault();
+  if (input.value === "") {
+    err.innerHTML = "Please add a link";
+    input.classList.add("border-red");
+  } else if (!validURL(input.value)) {
+    err.innerHTML = "Please add a valid link";
+    input.classList.add("border-red");
+  } else {
+    err.innerHTML = "";
+    input.classList.remove("border-red");
+    alert("Success");
+  }
 }
